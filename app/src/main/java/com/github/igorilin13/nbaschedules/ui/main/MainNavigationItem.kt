8529a92ba7309ce1f5f115e8cda709737dc52801
Igor Ilin <igorilindev@gmail.com>
2024-02-21 12:45:23 +0700

    Update libraries

--- app/src/main/java/com/github/igorilin13/nbaschedules/ui/main/MainNavigationItem.kt
+++ app/src/main/java/com/github/igorilin13/nbaschedules/ui/main/MainNavigationItem.kt
@@ -2,14 +2,14 @@ package com.github.igorilin13.nbaschedules.ui.main
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.github.igorilin13.nbaschedules.R
 
 enum class MainNavigationItem(val icon: ImageVector, @StringRes val label: Int, val route: String) {
     FAVORITE_TEAM(Icons.Filled.Favorite, R.string.navigation_favorite, "main_favorite"),
-    LEAGUE(Icons.Filled.List, R.string.navigation_league, "main_league"),
+    LEAGUE(Icons.AutoMirrored.Filled.List, R.string.navigation_league, "main_league"),
     SETTINGS(Icons.Outlined.Settings, R.string.navigation_settings, "main_settings"),
 }
